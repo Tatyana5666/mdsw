96c858485fdc84ce26cceec004c76b510f3b7e60
Alexander Tarima <atarima@my.wctc.edu>
2023-09-08 17:20:49 -0500

    Chapter4

--- sw.js
+++ sw.js
@@ -1,22 +1,42 @@
-// removed everything, copied from Tip Calculator project as per instructions in homework 2
+var urlList = [
+    '/',
+    '/css/fishcreek.css',
+    '/images/askthevet.gif',
+    '/images/fishcreeklogo.gif',
+    '/images/home.gif',
+    '/images/services.gif',
+    '/askvet.html',
+    '/favicon.ico',
+    '/index.html',
+    '/services.html'
+]
 
 self.addEventListener('install', event => {
-  console.log(`Event fired: ${event.type}`);
-  console.dir(event);
-});
+    console.log(`SW: ${event.type} even fired`)
+    event.waitUntil(
+        caches.open('pwa-learn-cache')
+        .then(cache => {
+            console.log('SW: Cache Opened')
+            return cache.addAll(urlList)
+        })
+        .catch(error => {
+            console.error(error)
+        })
 
-self.addEventListener('activate', event => {
-  console.log(`Event fired: ${event.type}`);
-  console.dir(event);
-});
-
-// This is what was needed for the homework. I removed what was previously in the fetch
-// just console.log and console.dir's (similar to console.log)
-// and added in the code that was in the homework 3 instructions
+    )
+})
 
 self.addEventListener('fetch', event => {
-  let ran= Math.floor(Math.random() * 3) + 1;
-  let ranPage = ran.toString() + '.html';
-  console.log(`Fetching random ${ranPage}`);
-  event.respondWith(fetch(ranPage));
-});
+    console.log(`SW: ${event.type} ${event.request.url}`)
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log(`SW: Return Cache ${event.request.url}`)
+                    return response
+                }
+                console.log(`SW: Return Network ${event.request.url}`)
+                return fetch(event.request)
+            })
+    )
+})
