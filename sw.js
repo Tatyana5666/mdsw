5a1feac5960cb1c6491b47ec613e45fc8ca39594
Alexander Tarima <atarima@my.wctc.edu>
2023-09-16 19:39:13 -0500

    Chapter 5 homework

--- sw.js
+++ sw.js
@@ -1,42 +1,34 @@
-var urlList = [
-    '/',
-    '/css/fishcreek.css',
-    '/images/askthevet.gif',
-    '/images/fishcreeklogo.gif',
-    '/images/home.gif',
-    '/images/services.gif',
-    '/askvet.html',
-    '/favicon.ico',
-    '/index.html',
-    '/services.html'
-]
-
-self.addEventListener('install', event => {
-    console.log(`SW: ${event.type} even fired`)
-    event.waitUntil(
-        caches.open('pwa-learn-cache')
-        .then(cache => {
-            console.log('SW: Cache Opened')
-            return cache.addAll(urlList)
-        })
-        .catch(error => {
-            console.error(error)
-        })
-
-    )
+self.addEventListener('sync', event => {
+    console.log('SW: sync event fired')
+    // Ignoring everything in the sync event because it deletes what is in the database
+    // But it does delete, if you want to remove the commenting
+    /*
+    if (event.tag === 'email') {
+        console.log('SW: Processing Email Sync');
+        event.waitUntil(
+            getEmailItems()
+                .then(data => {
+                    return Promise.all(
+                        // loop through the items array
+                        data.items.map(function (EmailItem) {
+                            //update the server if you can
+                            console.log(`SW: Updating Server ${EmailItem.value}`)
+                            //successfully posted
+                            // so whack the record at idx
+                            console.log(`SW: Deleting Email ${EmailItem.idx}`)
+                            return deleteEmail(data.db, EmailItem.idx)
+                        })
+                    )
+                }));
+            } else {
+        // this should never happen
+        console.log(`SW: Unrecognized sync event (${event.tag})`)
+    }
+     */
+    console.log('SW: sync event finished')
 })
 
-self.addEventListener('fetch', event => {
-    console.log(`SW: ${event.type} ${event.request.url}`)
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    console.log(`SW: Return Cache ${event.request.url}`)
-                    return response
-                }
-                console.log(`SW: Return Network ${event.request.url}`)
-                return fetch(event.request)
-            })
-    )
+self.addEventListener('install', event => {
+    console.log("SW: install event fired")
+    self.importScripts('./js/db.js')
 })
