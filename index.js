7bb1d613c8c0ae36dd8c7d82145c0c5451877ffc
Alexander Tarima <atarima@my.wctc.edu>
2023-10-16 09:34:48 -0500

    Chap7Hw

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-const express= require("express");
-const webpush= require("web-push");
-const bodyParser= require("body-parser");
-// path is for wokring with file and directory paths
-const path= require("path");
-// initialize express
-const app = express();
-app.use(bodyParser.json());
-// Set static path
-app.use(express.static(path.join(__dirname, "/")));
+navigator.serviceWorker.addEventListener('message', event => {
+    console.log('Message listener fired');
+    console.dir(event);
+}, false)
 
-const publicVapKey = 'BHos_KTEJ5BGTEKVR8uJfg3jEb_1ZZ-ZBcbza6O2ntvOMPo2aHLHEB-P7Tnimp_ExzWQUAO_me9_OzbGyinB9xc';
-const privateVapKey = 'LG4tfyrA21qyxSsKCMpLLsbmzp0DKRcuI-dtQM_EvAM';
+function sendMessage() {
+    console.log('sendMessage()')
+    const messageChannel = new MessageChannel();
+    messageChannel.port1.onmessage = function (event) {
+        console.log('Message received on channel')
+        console.dir(event.data)
+        document.getElementById("messageText").value=event.data;
+    };
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready
+            .then(registration => {
+                console.dir(navigator.serviceWorker.controller)
+                navigator.serviceWorker.controller.postMessage({
+                    action: 'messageSent',
+                    content: document.getElementById("messageText").value
+                }, [messageChannel.port2])
+            })
+            .catch(error => {
+                console.log('Service Worker not available')
+            })
+    }
 
-webpush.setVapidDetails(
-    "mailto:test@test.com",
-    publicVapKey,
-    privateVapKey
-)
+}
 
-//subscribe route
-app.post("/subscribe", (request, response) => {
-    //Get pushSubscription object
-    const subscription = request.body
-
-    // send 201 - resource created successfully
-    response.status(201).json({});
-
-    //create payload
-    const payload = JSON.stringify({title: "Push Test"})
-
-    //pass object into sendNotification
-    webpush
-        .sendNotification(subscription, payload)
-        .catch(err => console.error(err))
-});
-
-//run the server on port 5000
-const port = 5000
-app.listen(port, () => console.log(`Server started on port ${port}`))
-
+document.getElementById("btnMessage").addEventListener("click", sendMessage)
--- sw.js
+++ sw.js
@@ -1,41 +1,29 @@
+const version = 'v1'
+self.addEventListener('message', event => {
+    console.log(`SW: Message event fired: ${event.data}`)
+    console.dir(event)
+    if (typeof event.data.action !== 'undefined') {
+        console.log('SW: Action message received')
+        let theAction = event.data.action;
+        switch (theAction) {
+            case 'messageSent':
+                console.log('SW: Processing message')
+                event.ports[0].postMessage('message received')
+                break;
+            default:
+                console.log('SW: Unrecognized action')
+        }
+    } else {
+        console.log('SW: Not an action')
+    }
+})
 
 self.addEventListener('install', event => {
-    // fires when the browser installs the app
-    // here we're just logging the event and the contents
-    // of the object passed to the event. the purpose of this event
-    // is to give the service worker a place to setup the local 
-    // environment after the installation completes.
     console.log(`Event fired: ${event.type}`);
     console.dir(event);
 });
 
 self.addEventListener('activate', event => {
-    // fires after the service worker completes its installation. 
-    // It's a place for the service worker to clean up from previous 
-    // service worker versions
     console.log(`Event fired: ${event.type}`);
     console.dir(event);
 });
-
-self.addEventListener('fetch', event => {
-    // Fires whenever the app requests a resource (file or data)
-    // normally this is where the service worker would check to see
-    // if the requested resource is in the local cache before going
-    // to the server to get it. There's a whole chapter in the book
-    // covering different cache strategies, so I'm not going to say 
-    // any more about this here
-    console.log(`Fetching ${event.request.url}`);
-    // console.dir(event.request);
-    // Next, go get the requested resource from the network, 
-    // nothing fancy going on here.
-    event.respondWith(fetch(event.request));
-});
-
-self.addEventListener('push', event => {
-  console.log('SW: Push event fired');
-  console.dir(event);
-  const data = event.data.json();
-  console.dir(data);
-  self.registration.showNotification(data.title, data);
-});
-
